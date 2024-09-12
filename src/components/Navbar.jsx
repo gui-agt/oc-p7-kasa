cf9ef7aafcfd88d326f24229de46aab6c83255cb
gui-agt <gui.agt34@gmail.com>
2024-09-12 13:44:37 +0200

    patch error navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,29 @@ function Navbar() {
     return (
         <nav>
             <ul>
-                <li><NavLink to="/" exact activeClassName="active">Accueil</NavLink></li>
-                <li><NavLink to="/about" activeClassName="active">A propos</NavLink></li>
+                <li>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) => (isActive ? 'active' : '')}
+                    >
+                        Accueil
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/about"
+                        className={({ isActive }) => (isActive ? 'active' : '')}
+                    >
+                        A propos
+                    </NavLink>
+                </li>
             </ul>
         </nav>
     )
 }
 
 export default Navbar
+
+// Ancien code qui posait problème voir pk
+// <li><NavLink to="/" exact activeClassName="active">Accueil</NavLink></li>
+// <li><NavLink to="/about" activeClassName="active">A propos</NavLink></li>
